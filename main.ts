412284995ff35eef30a21cca27acb253cf56b589
MrR0ck <98587830+MrR0ck@users.noreply.github.com>
2022-11-28 21:00:07 +0200

    Обновить main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,21 +1,21 @@
 input.onButtonPressed(Button.A, function () {
-    let Вологість = 0
-    moistureReading = Math.map(pins.analogReadPin(AnalogPin.P0), 0, 1023, 0, 100)
+    Вологість = Math.map(pins.analogReadPin(AnalogPin.P0), 0, 1023, 0, 100)
     basic.showString("" + Math.round(Вологість) + "%")
     basic.clearScreen()
     if (Вологість > 70) {
-        _1.showColor(neopixel.colors(NeoPixelColors.Green))
-    }
-    if (Вологість > 70) {
-        _1.showColor(neopixel.colors(NeoPixelColors.Yellow))
-    }
-    if (Вологість > 70) {
-        _1.showColor(neopixel.colors(NeoPixelColors.Red))
+        strip.showColor(neopixel.colors(NeoPixelColors.Green))
+    } else {
+        if (Вологість > 30) {
+            strip.showColor(neopixel.colors(NeoPixelColors.Yellow))
+        }
+        if (Вологість < 30) {
+            strip.showColor(neopixel.colors(NeoPixelColors.Red))
+        }
     }
 })
-let moistureReading = 0
-let _1: neopixel.Strip = null
-let strip = neopixel.create(DigitalPin.P0, 3, NeoPixelMode.RGB)
-_1 = strip.range(0, 1)
+let Вологість = 0
+let strip: neopixel.Strip = null
+strip = neopixel.create(DigitalPin.P0, 3, NeoPixelMode.RGB)
+let _1 = strip.range(0, 1)
 let _2 = strip.range(1, 1)
 let _3 = strip.range(2, 1)
